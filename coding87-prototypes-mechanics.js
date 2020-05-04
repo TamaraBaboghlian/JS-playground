459ea26195a94f02cdf03613b44d815f9bfd2530
Tamar baboghlian <tamara.baboghlian@gmail.com>
2020-05-05 00:09:41 +0400

    reviewing prototypes

---
+++ coding87-prototypes-mechanics.js
@@ -0,0 +1,27 @@
+function NothingSpecial() {
+    console.log("Don't mind me!");
+}
+var a = new NothingSpecial();
+// "Don't mind me!"
+console.log(a())
+/*
+in JavaScript, it's most appropriate to say that a "constructor" is any function called
+with the new keyword in front of it.
+
+Functions aren't constructors, but function calls are "constructor calls" if and only if
+new is used.
+*/
+
+function Foo(name) {
+    this.name = name;
+}
+
+Foo.prototype.myName = function () {
+    return this.name;
+};
+
+var a = new Foo("a");
+var b = new Foo("b");
+
+a.myName(); // "a"
+b.myName(); // "b"
