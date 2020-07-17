c849f7ee51eef2afcff8008a0d07486b45e92db4
TamarBaboghlian <tamarbaboghlian@Tamars-MacBook-Pro.local>
2020-07-17 22:47:00 +0400

    concept of thunks

---
+++ coding93-thunks.js
@@ -0,0 +1,28 @@
+/*
+Thunk value is container of particular collection of state
+Async thunk is a function that doesn't need to pass any arg to do its job
+all I need is callback to pass and we get a value
+
+What incrediblly Powerful in this pattern: 
+From outside world we don't know nor do we have to care whether that value is immediately available or it may take
+awhile to get that a value
+*/
+
+function addAsync(x, y, cb) {
+    setTimeout(function(){
+        cb(x + y);
+    }, 1000)
+}
+
+var thunk = function(cb) {
+    addAsync(10, 15, cb);
+}
+
+thunk(function(sum) {
+    console.log(sum);
+})
+
+/*
+Thunks helps us undestand promises, since promises are time independent wrapper about value, just with fancy API
+*/
+
