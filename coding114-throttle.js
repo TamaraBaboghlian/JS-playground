5e63186c89396183b6e5c6f30072dc2bc6b9d7f3
TamarBaboghlian <tamarbaboghlian@Tamars-MacBook-Pro.local>
2020-11-14 01:33:36 +0400

    interesting example of throttle

---
+++ coding114-throttle.js
@@ -0,0 +1,25 @@
+function helloArmenia() {
+    console.log('Bonjour Armenia');
+}
+
+function throttle(fn, milliseconds) {
+    let lastCallTime;
+   
+
+    return function() {
+        console.log('*** throttle called ***');
+        let nowTime = Date.now();
+        if (!lastCallTime || lastCallTime < nowTime - milliseconds) {
+            lastCallTime = nowTime;
+            fn();
+        }
+    }
+}
+
+const helloArmeniaThrottled = throttle(helloArmenia, 1000);
+
+setInterval(helloArmeniaThrottled, 10);
+
+/*
+The most interesting part is: despite the 10ms interval, calls to helloArmenia are throttled and "Bonjour Armenia" will only be logged once per second.
+*/
