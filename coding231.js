62aed0b2197b1025106cd3fe67b401b2558b3d58
TamaraBaboyan <t.baboghlian@gmail.com>
2022-02-24 21:51:10 +0400

    promisify function

--- coding231.js
+++ coding231.js
@@ -31,17 +31,36 @@ attributeList.push(attributeEmail);
 attributeList.push(attributePhoneNumber);
 
 
-userPool.signUp('t.baboghlian@gmail.com', 'Password@123', attributeList, null, function(
-  err,
-  result
-) {
-  console.log("******** SUCCESSFULLY CREATED *********");
-  if (err) {
-    // alert(err.message || JSON.stringify(err));
-      console.log(err.message || JSON.stringify(err))
-    return;
-  }
-  const cognitoUser = result.user;
-  console.log('user name is ' + cognitoUser.getUsername());
-});
+// userPool.signUp('t.baboghlian@gmail.com', 'Password@123', attributeList, null, function(
+//   err,
+//   result
+// ) {
+//   console.log("******** SUCCESSFULLY CREATED *********");
+//   if (err) {
+//     // alert(err.message || JSON.stringify(err));
+//       console.log(err.message || JSON.stringify(err))
+//     return;
+//   }
+//   const cognitoUser = result.user;
+//   console.log('user name is ' + cognitoUser.getUsername());
+// });
 
+register(userPool, 't.baboghlian@gmail.com', 'Password@123', attributeList).then(console.log);
+
+// console.log(data);
+
+
+function register(userPool, email, password, attribute_list) {
+  return new Promise((resolve, reject) => {
+    userPool.signUp(email, password, attribute_list, null, (err, result) => {
+      console.log('inside');
+      if (err) {
+        console.log(err.message);
+        reject(err);
+        return;
+      }
+      cognitoUser = result.user;
+      resolve(cognitoUser)
+    });
+  });
+}
